9d5be88a7ac8c2ccffc164f242f67cff19330cc9
arammartirosyan1 <arammartirosyan788@gmail.com>
2023-10-21 21:19:18 +0400

    register, login, logout

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('cars', views.cars, name='cars'),
     path('<int:id>', views.detail, name='detail'),
     path('add_car', views.add_car, name='add_car'),
+    path('register', views.register, name='register'),
+    path('login_user', views.login_user, name='login_user'),
+    path('logout_user', views.logout_user, name='logout_user'),
+    path('home', views.home, name='home'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cars, CarsImage, AddCar, AddCarImage
+from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
@@ -21,6 +23,7 @@ def detail(request, id):
     })
 
 
+@login_required(login_url='login_user')
 def add_car(request):
     if request.method == 'POST':
         length = request.POST.get('length')
@@ -39,3 +42,36 @@ def add_car(request):
             )
 
     return render(request, 'main/add_car.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = UserForm()
+    return render(request, 'main/register.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'main/login_user.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login_user')
+
+
+@login_required(login_url='login_user')
+def home(request):
+    return render(request, 'main/home.html')
